5a43325fe50b8c35368899db5b45605f74c60dbc
Philip Felix <Philip.felix@zocdoc.com>
2014-10-06 11:42:46 -0400

    add R1,R7,R28, M calculations

---
+++ R7.sql
@@ -0,0 +1,19 @@
+--R1, R7, R28, Month and DueWeek calculations
+
+SELECT 
+	* 
+	, CASE  
+		when DATEPART(WEEKDAY,ActionTime)= 1 then CONVERT(date,DATEADD(DAY, -6, ActionTime))
+		when DATEPART(WEEKDAY,ActionTime)= 2 then convert(date,ActionTime)
+		when DATEPART(WEEKDAY,ActionTime)= 3 then convert(date,DATEADD(DAY, -1, ActionTime))
+		when DATEPART(WEEKDAY,ActionTime)= 4 then convert(date,DATEADD(DAY, -2, ActionTime))
+		when DATEPART(WEEKDAY,ActionTime)= 5 then convert(date,DATEADD(DAY, -3, ActionTime))
+		when DATEPART(WEEKDAY,ActionTime)= 6 then convert(date,DATEADD(DAY, -4, ActionTime))
+		when DATEPART(WEEKDAY,ActionTime)= 7 then convert(date,DATEADD(DAY, -5, ActionTime))
+	END AS Week
+	, -DATEDIFF(day, ActionTime, GETDATE()) AS R1
+	, -ceiling(convert(float, DATEDIFF(day, ActionTime, GETDATE())) / 7) AS R7
+	, -ceiling(convert(float, DATEDIFF(day, ActionTime, GETDATE())) / 28) AS R28
+	, -DATEDIFF(month, ActionTime, GETDATE()) AS M
+FROM #final6
+WHERE -ceiling(convert(float, DATEDIFF(day, ActionTime, GETDATE())) / 28) >= -12
