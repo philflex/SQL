31b132c885bca24a832bc5b56479c3f3d22524ae
Philip Felix <Philip.felix@zocdoc.com>
2014-10-06 11:26:44 -0400

    fix indent issue

--- Relative Time Zone Adjustment.sql
+++ Relative Time Zone Adjustment.sql
@@ -6,20 +6,20 @@
                         
 				WHEN 'columnidentifier'IN (SELECT #timezone.ProfessionalID FROM #timezone WHERE #timezone.timezoneID = 'US Eastern Standard Time')
                               
-					THEN DATEADD(hour, -(SELECT TOP 1 tz.BaseUTCOffset FROM table tz WHERE tz.TimeZoneID = (SELECT TOP 1 zc.TimeZoneID FROM table zc WHERE zc.ZipCode = (SELECT TOP 1 pl.ZIP FROM table pl WHERE pl.ID = (SELECT TOP 1 plm.ProviderLocationID FROM table plm WHERE plm.ProfessionalID = #final.CProfessionalId))))-5, PracticeInteraction) 
+			THEN DATEADD(hour, -(SELECT TOP 1 tz.BaseUTCOffset FROM table tz WHERE tz.TimeZoneID = (SELECT TOP 1 zc.TimeZoneID FROM table zc WHERE zc.ZipCode = (SELECT TOP 1 pl.ZIP FROM table pl WHERE pl.ID = (SELECT TOP 1 plm.ProviderLocationID FROM table plm WHERE plm.ProfessionalID = #final.CProfessionalId))))-5, PracticeInteraction) 
                               
-						WHEN 'columnidentifier' IN (SELECT #timezone.ProfessionalID FROM #timezone WHERE #timezone.timezoneID = 'Central Standard Time')
+				WHEN 'columnidentifier' IN (SELECT #timezone.ProfessionalID FROM #timezone WHERE #timezone.timezoneID = 'Central Standard Time')
                               
-							THEN DATEADD(hour, -(SELECT TOP 1 tz.BaseUTCOffset FROM table tz WHERE tz.TimeZoneID = (SELECT TOP 1 zc.TimeZoneID FROM table zc WHERE zc.ZipCode = (SELECT TOP 1 pl.ZIP FROM table pl WHERE pl.ID = (SELECT TOP 1 plm.ProviderLocationID FROM table plm WHERE plm.ProfessionalID = #final.CProfessionalId))))-7, PracticeInteraction) 
+			THEN DATEADD(hour, -(SELECT TOP 1 tz.BaseUTCOffset FROM table tz WHERE tz.TimeZoneID = (SELECT TOP 1 zc.TimeZoneID FROM table zc WHERE zc.ZipCode = (SELECT TOP 1 pl.ZIP FROM table pl WHERE pl.ID = (SELECT TOP 1 plm.ProviderLocationID FROM table plm WHERE plm.ProfessionalID = #final.CProfessionalId))))-7, PracticeInteraction) 
                         
-                        WHEN 'columnidentifier' IN (SELECT #timezone.ProfessionalID FROM #timezone WHERE #timezone.timezoneID = 'US Mountain Standard Time')                                               
+				WHEN 'columnidentifier' IN (SELECT #timezone.ProfessionalID FROM #timezone WHERE #timezone.timezoneID = 'US Mountain Standard Time')                                               
                         
-                  	THEN DATEADD(hour, -(SELECT TOP 1 tz.BaseUTCOffset FROM table tz WHERE tz.TimeZoneID = (SELECT TOP 1 zc.TimeZoneID FROM table zc WHERE zc.ZipCode = (SELECT TOP 1 pl.ZIP FROM table pl WHERE pl.ID = (SELECT TOP 1 plm.ProviderLocationID FROM table plm WHERE plm.ProfessionalID = #final.CProfessionalId))))-9, PracticeInteraction)  
+			THEN DATEADD(hour, -(SELECT TOP 1 tz.BaseUTCOffset FROM table tz WHERE tz.TimeZoneID = (SELECT TOP 1 zc.TimeZoneID FROM table zc WHERE zc.ZipCode = (SELECT TOP 1 pl.ZIP FROM table pl WHERE pl.ID = (SELECT TOP 1 plm.ProviderLocationID FROM table plm WHERE plm.ProfessionalID = #final.CProfessionalId))))-9, PracticeInteraction)  
 
-               WHEN 'columnidentifier' IN (SELECT #timezone.ProfessionalID FROM #timezone WHERE #timezone.timezoneID = 'Mountain Standard Time')                                               
+				WHEN 'columnidentifier' IN (SELECT #timezone.ProfessionalID FROM #timezone WHERE #timezone.timezoneID = 'Mountain Standard Time')                                               
                         
 			THEN DATEADD(hour, -(SELECT TOP 1 tz.BaseUTCOffset FROM table tz WHERE tz.TimeZoneID = (SELECT TOP 1 zc.TimeZoneID FROM table zc WHERE zc.ZipCode = (SELECT TOP 1 pl.ZIP FROM table pl WHERE pl.ID = (SELECT TOP 1 plm.ProviderLocationID FROM table plm WHERE plm.ProfessionalID = #final.CProfessionalId))))-9, PracticeInteraction) 
           
-       ELSE NULL
+		ELSE NULL
             
-    END AS 'AdjustedTimeZone'
+	END AS 'AdjustedTimeZone'
